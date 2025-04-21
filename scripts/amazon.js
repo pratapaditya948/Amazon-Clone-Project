83ebfa8d715d805c4d4b30b8e3cf1efb040635ed
Aditya Pratap <pratapaditya948@gmail.com>
2025-04-22 04:53:03 +0530

    Used products.js fiel

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products = [{
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090},
-{
-  name: 'Intermediate Size Basketball',
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  rating: {
-    stars: 4.0,
-    count: 127
-  },
-  priceCents: 2095
-},{
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}];
-
 let productsHTML = '';
 
 products.forEach((product) => {
